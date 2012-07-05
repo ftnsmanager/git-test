18014be6ed55469a2dea0d0e2d88c80f030d5900
Ermal Tureksi <ermal@ftns.co>
2012-07-05 04:15:03 -0400

    Updated index

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<?php
-require './functions.php';
-?>
+
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FTNS - World's 1st Fitness Radio!</title>
+	<title>test!</title>
 <style type="text/css">
 
 body {
@@ -127,77 +125,6 @@ body {
 
 </style>
 
-<link rel="stylesheet" href="/templates/system/css/ftns.css" />
-<link rel="stylesheet" href="/templates/system/css/jquery-ui-1.8.15.custom.css" type="text/css" />
-
-<!--
-commented sdalonzo, 2011-11-09
-to replace with v1.7
-<script src="../plugins/system/jquery/jquery/jquery-1.6.2.min.js"></script>
--->
-<script src="http://code.jquery.com/jquery-1.7.min.js"></script>
-
-<!-- non cdn libs -->
-<script type="text/javascript" src="js/amplify.min.js"></script>
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
-<script type="text/javascript" src="/templates/system/javascript/jquery-ui-1.8.15.custom.min.js"></script>
-<script type="text/javascript" src="/templates/system/javascript/jquery.tmpl.js"></script>
-<script type="text/javascript" src="/templates/system/javascript/underscore.js"></script>
-<script type="text/javascript" src="/templates/system/javascript/date.js"></script>
-<script type="text/javascript" src="jquery.timers-1.0.0.js"></script>
-
-<!-- app code -->
-<script type="text/javascript" src="js/ftns-calendar.js"></script>
-
-<!-- google gdata api load -->
-<script type="text/javascript">
-	google.load("gdata", "2.x");
-	google.setOnLoadCallback(FTNS.getSchedule);
-</script>
-
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-22549252-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-
-<script type="text/javascript">
-$(document).ready(function(){
-	$(document).ready(function() {
-		$(".liveTime").everyTime(1000,function(i){
-			$.ajax({
-			  url: "liveTime.php",
-			  cache: false,
-			  success: function(html){
-				$(".liveTime").html(html);
-			  }
-			})
-		})
-	});
-   $('.liveTime').css({});
-});
-
-</script>
-
-
-<!-- DOUBLE CLICK -->
-<script type='text/javascript' src='http://partner.googleadservices.com/gampad/google_service.js'></script>
-<script type='text/javascript'>
-	GS_googleAddAdSenseService("ca-pub-7173060921064171");
-	GS_googleEnableAllServices();
-</script>
-<script type='text/javascript'>
-	GA_googleAddSlot("ca-pub-7173060921064171", "FTNS_InnerPage_Header_468x60");
-</script>
-<script type='text/javascript'>
-	GA_googleFetchAds();
-</script>
 
 
 
@@ -210,12 +137,7 @@ $(document).ready(function(){
 	
 	<div class="liveTime">LIVE TIME</div>
 
-	<div class="info">
-		<?php
-		$player_feed = new Schedule('player', date('l'));
-		$player_feed->parse_day();
-		?>
-	</div>
+
 
 	<img id="onAirLogo" src="images/shows/default.png" />
 	<div id="listenLiveOnAir">
